c12e3d13f924e0dea483fb6469423b16f9f4e97e
gribeiroantunes <gribeiroantunes@gmail.com>
2026-04-02 12:35:29 -0300

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -51,6 +51,7 @@ def get_matches():
                     "prob": prob_over_25 / 100,
                     "confidence": confidence,
                     "teams": teams
+                    "event_date": event.get("event_date")
                 })
 
             prob_btts_yes = p.get("prob_btts_yes")
@@ -60,6 +61,7 @@ def get_matches():
                     "prob": prob_btts_yes / 100,
                     "confidence": confidence,
                     "teams": teams
+                    "event_date": event.get("event_date")
                 })
 
         except (KeyError, TypeError, ValueError):
