240833768aeb5cf616f1f3655534e11099d1f8e5
gribeiroantunes <gribeiroantunes@gmail.com>
2026-04-02 10:56:10 -0300

    Refactor configuration settings into CONFIG dictionary

--- config.py
+++ config.py
@@ -1,18 +1,5 @@
-# config.py
-
-HISTORY_FILE = "bet_history.json"
-
-ODDS_MEDIA = {
-    "over_2_5": 1.75,
-    "over_1_5": 1.35,
-    "btts": 1.80,
-    "under_2_5": 1.85
+CONFIG = {
+    "min_prob": 0.60,
+    "min_ev": 0.05,
+    "bankroll": 1000
 }
-
-EV_MIN_FREE = 0.05
-EV_MIN_VIP = 0.10
-
-MIN_GAMES_FOR_LEARNING = 10
-
-# corte automático de ligas ruins
-MIN_LEAGUE_PERFORMANCE = 0.48
