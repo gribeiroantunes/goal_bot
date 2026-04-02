a9f88ee7790c04246f7056d5ed5109bee20c07ca
gribeiroantunes <gribeiroantunes@gmail.com>
2026-04-02 10:38:05 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+HISTORY_FILE = "bet_history.json"
+
+ODDS_MEDIA = {
+    "over_2_5": 1.75,
+    "over_1_5": 1.35,
+    "btts": 1.80,
+    "under_2_5": 1.85
+}
+
+EV_MIN_FREE = 0.05
+EV_MIN_VIP = 0.10
+
+MIN_GAMES_FOR_LEARNING = 10
