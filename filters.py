85826b3eec67041b5ab673b23ebac373af0ad2a1
gribeiroantunes <gribeiroantunes@gmail.com>
2026-04-02 11:24:47 -0300

    Refactor filter_bets to include side data directly

--- filters.py
+++ filters.py
@@ -2,11 +2,9 @@ def filter_bets(value_data):
     bets = []
 
     for side in ["over", "under"]:
-        data = value_data[side]
-
         bets.append({
             "type": side,
-            **data
+            **value_data[side]
         })
 
     return bets
